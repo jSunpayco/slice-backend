4e114aee3243053dd95c632c7004d95133b6a7f1
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-22 00:27:32 -0700

    Enable cors for my apps

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://slice-recipes.vercel.app/']
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 const PORT = 8080
