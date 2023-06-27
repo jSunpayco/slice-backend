e744f2354e293916448799720e675a708185ae49
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-26 21:57:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://slice-recipes.vercel.app/']
+  origin: ['http://localhost:3000', 'https://slice-recipes.vercel.app']
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
