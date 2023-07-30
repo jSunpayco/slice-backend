55f39ad90b97a1bbc1e77868081bdf79063dab3b
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-07-29 23:36:14 -0700

    Fix validation

--- validation.js
+++ validation.js
@@ -2,9 +2,32 @@ const { body } = require('express-validator');
 
 const validAllergens = ["Fish", "Shellfish", "Milk", "Eggs", "Peanuts", "Tree Nuts", "Soy", "Gluten"];
 const validCourses = ["Starter", "Breakfast", "Main Dish", "Side Dish", "Dessert"];
-const validProteinsMeat = ["Beef", "Pork", "Poultry", "Fish", "Shellfish", "Vegan", "Vegetarian"];
+const validProteinsMeat = ["Beef", "Pork", "Poultry", "Fish", "Shellfish"];
 const validProteinsVeg = ["Vegan", "Vegetarian"];
 const validServings = ["1", "2", "3-4", "5-6", "7-10", "11+"];
+
+const notIn = (values, validArr, message) => {
+  const validSet = new Set(validArr)
+  if(typeof values == "string"){
+    if(!validSet.has(values))
+      throw new Error(message);
+  }else{
+    for(const val of values){
+      if(!validSet.has(val))
+        throw new Error(message);
+    }
+  }
+  return true;
+}
+
+const proteinValid = (values, validArr) => {
+  const validSet = new Set(validArr)
+  for(const val of values){
+    if(!validSet.has(val))
+      return false;
+  }
+  return true;
+}
 
 exports.validateRecipe = [
     body('name')
@@ -22,36 +45,34 @@ exports.validateRecipe = [
   body('allergens')
     .optional()
     .isArray().withMessage('Allergens has an invalid format') // must be array
-    .isIn([validAllergens]).withMessage('Invalid allergens detected'),
+    .custom((values) => notIn(values, validAllergens, 'Invalid allergens detected'))
+    .trim(),
   body('course')
     .notEmpty().withMessage('Course is required')
-    .isIn([validCourses]).withMessage('Invalid course detected')
+    .custom((values) => notIn(values, validCourses, 'Invalid course detected'))
     .trim(),
   body('protein')
     .notEmpty().withMessage('Protein is required')
     .isArray().withMessage('Protein has an invalid format') // must be array
-    .custom((value) => {
-      const validProtein = value.some((protein) =>
-        validProteinsMeat.includes(protein) || validProteinsVeg.includes(protein)
-      );
+    .custom((values) => {
+      const isVeg = proteinValid(values, validProteinsVeg);
+      const isMeat = proteinValid(values, validProteinsMeat);
 
-      if (!validProtein)
-        throw new Error('Invalid protein detected');
+      if(isMeat == false){
+        if(isVeg == false)
+          throw new Error('Invalid protein detected');
+      }else{
+        if(isMeat == false)
+          throw new Error('Invalid protein detected');
+      }
 
-      const inBothProtein = value.some((protein) =>
-        validProteinsMeat.includes(protein) && validProteinsVeg.includes(protein)
-      );
-
-      if (inBothProtein)
-        throw new Error('Protein must either contain meat or not');
-
-      if(value.length === 2 && validProteinsVeg.includes(value[0]) && validProteinsVeg.includes(value[1]))
+      if(isVeg == true && values.length === 2)
         throw new Error('Protein cannot be both Vegetarian and Vegan');
 
       return true;
     }),
-    body('servings')
-      .notEmpty().withMessage('Serving is required')
-      .isIn([validServings]).withMessage('Invalid serving detected')
-      .trim(),
+  body('servings')
+    .notEmpty().withMessage('Serving is required')
+    .custom((values) => notIn(values, validServings, 'Invalid serving detected'))
+    .trim(),
 ];
